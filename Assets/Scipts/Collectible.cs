3cc499039029ccbf747b9d3a32e1c272019bf9c7
Travis Chandler <travestychandler@gmail.com>
2017-05-15 22:09:19 -0400

    Added in files to handle basic game functionality. Player can move left and right, as well as jump. The player's breath meter exists and is increase and decreased by movement as expected. Two collectibles exist in the scene,  that increase or decrease the player's meter.

---
+++ Assets/Scipts/Collectible.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collectible : MonoBehaviour {
+    public enum CollectibleType
+    {
+        Good,
+        Bad,
+        Invalid
+    }
+
+    public CollectibleType collType = CollectibleType.Good;
+    public float collectibleBreathChange = 20f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scipts/PlayerController.cs
@@ -0,0 +1,106 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour {
+    public float movementSpeed = 5f;
+    public float stopSpeed = 0.5f;
+    public float jumpSpeed = 5f;
+    public bool canJump = true;
+    public LayerMask groundLayers;
+    public Transform circleCastPosition;
+    public float circleCastRadius = 0.5f;
+    private Rigidbody2D rb;
+
+    public float breathMeter = 100f;
+    public float breathChange = 0.5f;
+
+    public Slider breathSlider;
+	// Use this for initialization
+	void Start () {
+        rb = this.GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(IsGrounded() && canJump)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                Jump();
+            }
+        }
+        if (IsGrounded())
+        {
+            if (Input.GetKeyUp(KeyCode.D))
+            {
+                rb.velocity = new Vector2(stopSpeed, rb.velocity.y);
+            }
+            if (Input.GetKeyUp(KeyCode.A))
+            {
+                rb.velocity = new Vector2(-stopSpeed, rb.velocity.y);
+            }
+            if (Input.GetKey(KeyCode.D))
+            {
+                rb.velocity = new Vector2(movementSpeed, rb.velocity.y);
+            }
+            if (Input.GetKey(KeyCode.A))
+            {
+                rb.velocity = new Vector2(-movementSpeed, rb.velocity.y);
+            }
+        }
+        Debug.Log(rb.velocity.x);
+        if(Mathf.Abs(rb.velocity.x) > 0.1f)
+        {
+           breathMeter = Mathf.Clamp(breathMeter - (Time.deltaTime * breathChange), 0f, 100f);
+        }
+        else
+        {
+           breathMeter = Mathf.Clamp(breathMeter + (Time.deltaTime * breathChange), 0f, 100f);
+        }
+        breathSlider.value = breathMeter;
+	}
+
+    public bool IsGrounded()
+    {
+        return (Physics2D.OverlapCircle(circleCastPosition.position, circleCastRadius, groundLayers)) != null;
+    }
+
+    public void Jump()
+    {
+        canJump = false;
+        rb.velocity += new Vector2(0f, jumpSpeed);
+        StartCoroutine(JumpWait());
+    }
+
+    public IEnumerator JumpWait()
+    {
+        float timer = 0f;
+        float maxTime = 0.1f;
+        while (timer < maxTime)
+        {
+            timer += Time.deltaTime;
+            yield return null;
+        }
+        canJump = true;
+    }
+
+    public void OnTriggerEnter2D(Collider2D col)
+    {
+        if(col.tag == "collectible")
+        {
+            GameObject game = col.gameObject;
+            Collectible collect = game.GetComponent<Collectible>();
+            if(collect.collType == Collectible.CollectibleType.Good)
+            {
+                breathMeter += collect.collectibleBreathChange;
+            }
+            if (collect.collType == Collectible.CollectibleType.Bad)
+            {
+                breathMeter -= collect.collectibleBreathChange;
+            }
+            Destroy(game);
+        }
+    }
+}

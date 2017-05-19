2ff13962f012c9bd6d196adb24e1314600fa234a
Travis Chandler <travestychandler@gmail.com>
2017-05-18 23:23:48 -0400

    Implemented camera follow, win and loss states.

--- Assets/Scipts/PlayerController.cs
+++ Assets/Scipts/PlayerController.cs
@@ -5,6 +5,16 @@ using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour {
+
+    public enum GamePhase
+    {
+        Playing,
+        Victory,
+        GameOver,
+        Invalid
+    }
+
+    public GamePhase currentPhase;
     public float movementSpeed = 5f;
     public float stopSpeed = 0.5f;
     public float jumpSpeed = 5f;
@@ -16,24 +26,34 @@ public class PlayerController : MonoBehaviour {
 
     public float breathMeter = 100f;
     public float breathChange = 0.5f;
-
+    public RectTransform gameOverPanel;
+    public RectTransform levelCompletePanel;
     public Slider breathSlider;
 	// Use this for initialization
 	void Start () {
+        currentPhase = GamePhase.Playing;
         rb = this.GetComponent<Rigidbody2D>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        if(IsGrounded() && canJump)
+        
+        if (currentPhase == GamePhase.Playing)
         {
-            if (Input.GetKeyDown(KeyCode.Space))
+            if (breathMeter <= 0f)
             {
-                Jump();
+                currentPhase = GamePhase.GameOver;
+                GameOver();
+                return;
             }
-        }
-        if (IsGrounded())
-        {
+            if (IsGrounded() && canJump)
+            {
+                if (Input.GetKeyDown(KeyCode.Space))
+                {
+                    Jump();
+                }
+            }
+            float speedMutliplier = (IsGrounded()) ? 1f : 0.5f;
             if (Input.GetKeyUp(KeyCode.D))
             {
                 rb.velocity = new Vector2(stopSpeed, rb.velocity.y);
@@ -44,28 +64,60 @@ public class PlayerController : MonoBehaviour {
             }
             if (Input.GetKey(KeyCode.D))
             {
-                rb.velocity = new Vector2(movementSpeed, rb.velocity.y);
+                rb.velocity = new Vector2(movementSpeed * speedMutliplier, rb.velocity.y);
             }
             if (Input.GetKey(KeyCode.A))
             {
-                rb.velocity = new Vector2(-movementSpeed, rb.velocity.y);
+                rb.velocity = new Vector2(-movementSpeed * speedMutliplier, rb.velocity.y);
+            }
+            Debug.Log(rb.velocity.x);
+            if (Mathf.Abs(rb.velocity.x) > 0.1f)
+            {
+                breathMeter = Mathf.Clamp(breathMeter - (Time.deltaTime * breathChange), 0f, 100f);
+            }
+            else
+            {
+                breathMeter = Mathf.Clamp(breathMeter + (Time.deltaTime * breathChange), 0f, 100f);
+            }
+            breathSlider.value = breathMeter;
+            if (Input.GetKeyDown(KeyCode.Backslash))
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().name);
             }
         }
-        Debug.Log(rb.velocity.x);
-        if(Mathf.Abs(rb.velocity.x) > 0.1f)
-        {
-           breathMeter = Mathf.Clamp(breathMeter - (Time.deltaTime * breathChange), 0f, 100f);
-        }
-        else
-        {
-           breathMeter = Mathf.Clamp(breathMeter + (Time.deltaTime * breathChange), 0f, 100f);
-        }
-        breathSlider.value = breathMeter;
-        if (Input.GetKeyDown(KeyCode.Backslash))
-        {
-            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
-        }
 	}
+    public void LevelComplete()
+    {
+        StartCoroutine(LevelCompleteScreen());
+    }
+
+    public IEnumerator LevelCompleteScreen()
+    {
+        float timer = 0f;
+        while (timer < .5f)
+        {
+            float scaleVal = Mathf.Lerp(0f, 1f, timer / 0.5f);
+            levelCompletePanel.localScale = Vector3.one * scaleVal;
+            timer += Time.deltaTime;
+            yield return null;
+        }
+    }
+
+    public void GameOver()
+    {
+        StartCoroutine(ShowGameOverScreen());
+    }
+    public IEnumerator ShowGameOverScreen()
+    {
+        float timer = 0f;
+        while(timer < .5f)
+        {
+            float scaleVal = Mathf.Lerp(0f, 1f, timer / 0.5f);
+            gameOverPanel.localScale = Vector3.one * scaleVal;
+            timer += Time.deltaTime;
+            yield return null;
+        }
+    }
 
     public bool IsGrounded()
     {
@@ -106,6 +158,11 @@ public class PlayerController : MonoBehaviour {
                 breathMeter -= collect.collectibleBreathChange;
             }
             Destroy(game);
+        }
+        if(col.tag == "endoflevel")
+        {
+            currentPhase = GamePhase.Victory;
+            LevelComplete();
         }
     }
 }

de030152a1a352bab4edccea0375fc64771c0c7b
mrm11008 <Max@n3-118-11.dhcp.drexel.edu>
2017-05-25 13:45:18 -0400

    dog state skeleton

--- Assets/DogController.cs
+++ Assets/DogController.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 
 public class DogController : MonoBehaviour {
 
+	private enum STATE {RUN, STAY};
+	STATE currentState;
+
+	public float runTimer;
+	public float stayTimer;
+
 	// Use this for initialization
 	void Start () {
 		
@@ -11,6 +17,39 @@ public class DogController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+		switch (currentState) {
+		case STATE.RUN:
+			UpdateRun ();
+			break;
+		case STATE.STAY:
+			UpdateStay ();
+			break;
+		}
+	}
+
+	private void EnterRunState() {
+		currentState = STATE.RUN;
+		runTimer = Random.Range (1f, 3f);
+		UpdateRun ();
+	}
+	private void UpdateRun() {
+		runTimer -= Time.deltaTime;
+
+		if (runTimer <= 0f) {
+			EnterStayState ();
+		}
+	}
+
+	private void EnterStayState() {
+		currentState = STATE.STAY;
+		stayTimer = Random.Range (1f, 5f);
+		UpdateStay ();
+	}
+	private void UpdateStay() {
+		stayTimer -= Time.deltaTime;
+
+		if (stayTimer <= 0f) {
+			EnterRunState ();
+		}
 	}
 }

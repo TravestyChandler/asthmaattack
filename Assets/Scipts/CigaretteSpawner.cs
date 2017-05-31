e886d8ce2e953f646cc28dc9bef06faf17f132a2
Travis Chandler <travestychandler@gmail.com>
2017-05-30 22:08:45 -0400

    Implemented cigarette obstacle

---
+++ Assets/Scipts/CigaretteSpawner.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CigaretteSpawner : MonoBehaviour {
+
+    public float heightRange = 1f;
+    public float spawnTimer = 2.5f;
+    public GameObject cigarettePrefab;
+    private float internalTimer = 0.0f;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(spawnTimer < internalTimer)
+        {
+            internalTimer = 0f;
+            SpawnCigarette();
+        }
+        else
+        {
+            internalTimer += Time.deltaTime;
+        }
+	}
+
+    public void SpawnCigarette()
+    {
+        float rand = Random.Range(this.transform.position.y - heightRange, this.transform.position.y + heightRange);
+        Vector3 pos = this.transform.position;
+        pos.y = rand;
+        GameObject cig = Instantiate(cigarettePrefab, pos, Quaternion.identity);
+        Destroy(cig, 10f);
+    }
+}

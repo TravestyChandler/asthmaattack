9583b192c8650a2f55ce63f379b222ea592546ed
mrm11008 <Max@n3-118-11.dhcp.drexel.edu>
2017-06-04 12:47:47 -0400

    initial start screen
    Pause screen?

---
+++ Assets/GM.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GM : MonoBehaviour {
+
+	public static GM instance = null;
+	public GameObject startScreen;
+
+	void Awake() {
+		if (instance == null)
+			instance = this;
+		else if (instance != this)
+			Destroy (gameObject);
+	}
+
+	// Use this for initialization
+	void Start () {
+		startScreen.SetActive (true);
+		Time.timeScale = 0.0f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	public void StartGame() {
+		startScreen.SetActive (false);
+		Time.timeScale = 1.0f;
+	}
+}
